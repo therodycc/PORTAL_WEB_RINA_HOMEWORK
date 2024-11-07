580cf64cb037965c46d13e47cc1cf28da086cb33
Mbernabel56 <162064911+Mbernabel56@users.noreply.github.com>
2024-11-07 16:31:14 -0400

    Escribi sec 12

--- src/components/Section1.tsx
+++ src/components/Section1.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Section1 = () => {
     return (
         <div>
-            <h3 className="text-slate-400 text-[5rem] font-bold italic">Sección 1</h3>
+            <h3 className="text-slate-400 text-[5rem] font-bold italic">Sección 12</h3>
         </div>
     )
 }
